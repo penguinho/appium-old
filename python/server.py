61e32d46413fe9370a24a293fadb5c5425cbed3c
Jason Huggins <jason@jrandolph.com>
2012-10-16 12:14:21 -0700

    Added more WebDriver protocol functionality.

--- python/server.py
+++ python/server.py
@@ -1,8 +1,9 @@
 from applecart import Applecart
-from bottle import Bottle, request, response
+from bottle import Bottle, request, response, redirect
 from bottle import run, static_file
 import json
 from time import time
+from time import sleep
 
 app = Bottle()
 
@@ -10,32 +11,40 @@ app = Bottle()
 def get_favicon():
     return static_file('favicon.ico', root='.')
 
-@app.route('/status', method='GET')
+@app.route('/wd/hub/status', method='GET')
 def status():
     status = {'sessionId': None, 
               'status': 0, 
               'value': {'build': {'version': 'Applecart 1.0'}}}
     return status 
 
-@app.route('/session', method='POST')
+@app.route('/wd/hub/session', method='POST')
 def create_session():
-#TODO: Get app name from desired caps and start with applecart here
-#    app.ios_client = Applecart('/path/to/your/awesome.app')
-#    app.ios_client.start() 
+    redirect('/wd/hub/session/1')
+
+@app.route('/wd/hub/session/<session_id>', method='GET')
+def get_session(session_id=''):
     response = {'sessionId': '1', 
                 'status': 0, 
-                'value': None}
-    return response 
+                'value': {"version":"5.0",
+                          "webStorageEnabled":False,
+                          "locationContextEnabled":False,
+                          "browserName":"iOS",
+                          "platform":"MAC",
+                          "javascriptEnabled":True,
+                          "databaseEnabled":False,
+                          "takesScreenshot":False}}
+    return response
 
-@app.route('/session/<session_id>', method='DELETE')
+@app.route('/wd/hub/session/<session_id>', method='DELETE')
 def delete_session(session_id=''):
     app.ios_client.stop() 
     response = {'sessionId': '1', 
                 'status': 0, 
-                'value': None}
+                'value': {}}
     return response 
 
-@app.route('/session/<session_id>/frame', method='POST')
+@app.route('/wd/hub/session/<session_id>/frame', method='POST')
 def switch_to_frame(session_id=''):
     status = 0
     request_data = request.body.read()
@@ -49,27 +58,138 @@ def switch_to_frame(session_id=''):
         response.status = 400
         status = 13  # UnknownError
 
-    response = {'sessionId': '1', 
+    app_response = {'sessionId': '1', 
                 'status': status, 
                 'value': {}}
-    return response 
+    return app_response 
 
-@app.route('/session/<session_id>/element', method='POST')
-def find_element(session_id=''):
+@app.route('/wd/hub/session/<session_id>/execute', method='POST')
+def execute_script(session_id=''):
     status = 0
+    ios_response = ''
     request_data = request.body.read()
     try:
+        script = json.loads(request_data).get('script')
+        ios_response = app.ios_client.proxy(script)[0][1]
+    except:
+        response.status = 400
+        status = 13  # UnknownError
+
+    response = {'sessionId': '1', 
+		'status': status, 
+		'value': ios_response}
+    return response
+    
+@app.route('/wd/hub/session/<session_id>/element/<element_id>/text', method='GET')
+def get_text(session_id='', element_id=''):
+    status = 0
+    ios_response = ''
+    try:
+        script = "elements['%s'].value()" % element_id
+        ios_response = app.ios_client.proxy(script)[0][1]
+    except:
+        response.status = 400
+        status = 13  # UnknownError
+
+    response = {'sessionId': '1', 
+		'status': status, 
+		'value': ios_response}
+    return response
+
+@app.route('/wd/hub/session/<session_id>/element/<element_id>/attribute/<attribute>', method='GET')
+def get_text(session_id='', element_id='', attribute=''):
+    status = 0
+    ios_response = ''
+    try:
+        script = "elements['%s'].name()" % element_id
+        ios_response = app.ios_client.proxy(script)[0][1]
+    except:
+        response.status = 400
+        status = 13  # UnknownError
+
+    response = {'sessionId': '1', 
+		'status': status, 
+		'value': ios_response}
+    return response
+@app.route('/wd/hub/session/<session_id>/element/<element_id>/click', method='POST')
+def get_text(session_id='', element_id=''):
+    status = 0
+    ios_response = ''
+    try:
+        script = "elements['%s'].tap()" % element_id
+        ios_response = app.ios_client.proxy(script)[0][1]
+    except:
+        response.status = 400
+        status = 13  # UnknownError
+
+    response = {'sessionId': '1', 
+		'status': status, 
+		'value': ios_response}
+    return response
+
+@app.route('/wd/hub/session/<session_id>/element/<element_id>/value', method='POST')
+def set_value(session_id='', element_id=''):
+    status = 0
+    ios_response = ''
+    request_data = request.body.read()
+    print request_data
+    try:
+        value_to_set = json.loads(request_data).get('value') 
+        value_to_set = ''.join(value_to_set)
+
+        script = "elements['%s'].setValue('%s')" % (element_id, value_to_set) 
+        print script
+        ios_response = app.ios_client.proxy(script)[0][1]
+    except:
+        response.status = 400
+        status = 13  # UnknownError
+
+    response = {'sessionId': '1', 
+		'status': status, 
+		'value': ''}
+    return response
+
+@app.route('/wd/hub/session/<session_id>/elements', method='POST')
+def find_elements(session_id=''):
+    status = 0
+    request_data = request.body.read()
+    print request_data
+    try:
         locator_strategy = json.loads(request_data).get('using')
-        element_id = json.loads(request_data).get('value') 
-        element_var_name = 'wde' + str(int(time() * 1000000))
+        element_type = json.loads(request_data).get('value') 
+        elements = {'button': 'buttons()',
+                    'textField': 'textFields()',
+                    'secureTextField': 'secureTextFields()'} 
+
+        ios_request = "wd_frame.%s.length" % elements[element_type]
+        print ios_request
+
+        number_of_items = int(app.ios_client.proxy(ios_request)[0][1])
+        found_elements = []
+        print number_of_items
+        for i in range(number_of_items):
+            var_name = 'wde' + str(int(time() * 1000000))
+            print var_name
+
+            ios_request = "elements['%s'] = wd_frame.%s[%s]" % (var_name, elements[element_type], i)
+            print ios_request
+
+            ios_response = app.ios_client.proxy(ios_request)
+            print ios_response
+            found_elements.append({'ELEMENT':var_name})
     except:
         response.status = 400
         status = 13  # UnknownError
     response = {'sessionId': '1', 
                 'status': status, 
-                'value': {'message':'Unknown Error'}}
+                'value': found_elements}
     return response 
     
 
+#app.ios_client = Applecart('/path/to/your/awesome.app')
+#app.ios_client.start() 
+#app.ios_client.proxy('wd_frame = mainWindow')
+#app.ios_client.proxy('elements = {}')
+
 #run(app, server='paste', host='0.0.0.0', port=8080, reloader=True)
-run(app, host='0.0.0.0', port=8080, reloader=True)
+#run(app, host='0.0.0.0', port=4723)
